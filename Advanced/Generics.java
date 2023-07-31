b2e2aae47f54b8dff6244275f7029f32796a50dc
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-07-31 17:08:14 +0530

    Generics implemented

--- Advanced/Generics.java
+++ Advanced/Generics.java
@@ -1,3 +1,27 @@
-public class Generics {
+import java.util.*;
 
+class Student {
+    private String name;
+    private int roll;
+}
+
+class Employee {
+    private String name;
+    private int roll;
+}
+
+public class Generics {
+    public static void main(String[] args) {
+        Student s = new Student();
+        Student s1 = new Student();
+        Employee e = new Employee();
+        Employee e1 = new Employee();
+        ArrayList<Student> al = new ArrayList<Student>();
+        al.add(s);
+        al.add(s1);
+        System.out.println(al);
+        // al.add(e); //cannot insert an employee type of object
+        // Generics ensure that type safety is enforced in a collection since they're
+        // dynamic and heterogenous in nature
+    }
 }
