8f7b428fd27d7679d61c8f7e1c9d4e82dafaf1ae
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-07-31 16:38:06 +0530

    tree map hashtables added

--- Advanced/Hashmap.java
+++ Advanced/Hashmap.java
@@ -20,5 +20,19 @@ public class Hashmap {
         // in linked hash map the order of insertion is preserved
         // null keys and values are allowed duplicate values are permitted but keys have
         // to be unique
+        Hashtable ht = new Hashtable();
+        ht.put(1, "google");
+        ht.put(2, "microsoft");
+        ht.put(3, "goldman sachs");
+        // ht.put(null, "goldman sachs");
+        // the above one throws an exception null pointer , the key cannot be null
+        System.out.println("hashtable " + ht);
+        ht.putIfAbsent(4, "Mozilla"); // the position is empty then only insert this one
+        System.out.println("hashtable " + ht);
+        TreeMap tm = new TreeMap();
+        Integer i = new Integer(1);
+        tm.put(i, "chrome");
+        System.out.println("treemap " + tm);
+        // we can pass an integer object has a key to a tree map
     }
 }
