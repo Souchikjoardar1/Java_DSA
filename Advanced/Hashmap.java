2324b07e39c18ddaa2b423e1ec3e52e752c026f0
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-07-31 13:50:25 +0530

    Maps added

--- Advanced/Hashmap.java
+++ Advanced/Hashmap.java
@@ -1,7 +1,24 @@
-package Advanced;
+import java.util.*;
 
 public class Hashmap {
     public static void main(String[] args) {
-
+        Hashtable hm = new Hashtable();
+        hm.put(1, "souchik");
+        hm.put(2, "joardar");
+        hm.put(3, "beast");
+        System.out.println(hm);
+        HashMap h = new HashMap();
+        h.put("rohit", "dude");
+        h.put("Chikowski", "Alfredo");
+        h.put("kacey", "musgraves");
+        h.put(null, null);
+        System.out.println(h);
+        LinkedHashMap l = new LinkedHashMap();
+        l.put(10, "easy g");
+        l.put(1, "Eminem");
+        System.out.println(l);
+        // in linked hash map the order of insertion is preserved
+        // null keys and values are allowed duplicate values are permitted but keys have
+        // to be unique
     }
 }
