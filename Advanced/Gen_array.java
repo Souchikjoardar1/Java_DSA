65572154d814aa22d3cdf61bd01a3cdb56213f11
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-08-07 22:05:32 +0530

    Experimental generics Type of array

--- Advanced/Gen_array.java
+++ Advanced/Gen_array.java
@@ -4,17 +4,18 @@ import java.util.*;
 class G<T> {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    Object[] a = (T[]) new Object[n];// converting array a from Object type to a generic type <T> by downcasting
+    ArrayList<T> list = new ArrayList<>();
 
     G() {
         System.out.println("enter the array elements");
         for (int i = 0; i < n; i++) {
-            this.a[i] = in.nextInt();
+            Integer e = in.nextInt();
+            list.add((T) e);
         }
-        toString((T[]) a);// converting array a from Object type to a generic type <T> by downcasting
+        printarray(list);
     }
 
-    public void toString(T x[]) {
+    public void printarray(ArrayList<T> x) {
         System.out.println("the array elements are:");
         for (T a : x) {
             System.out.println(a);
@@ -22,10 +23,9 @@ class G<T> {
     }
 }
 
-@SuppressWarnings("unchecked")
 public class Gen_array {
     public static void main(String[] args) {
         System.out.println("enter the size of the array");
-        new <Integer>G();
+        new G<Integer>();
     }
 }
---
+++ Advanced/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+Integer e = in.nextInt();
