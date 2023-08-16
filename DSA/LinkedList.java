1d810752677557893e558c87de30928320668506
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-08-16 22:20:43 +0530

    appended an element to the linkedlist

--- DSA/LinkedList.java
+++ DSA/LinkedList.java
@@ -19,8 +19,45 @@ public class LinkedList {
         length = 1;
     }
 
-    public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
 
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("length: " + length);
+
+    }
+
+    public void append(int value) {
+        Node newnode = new Node(value);
+        if (length == 0) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail = newnode;
+        }
+        length++;
+    }
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+        myLinkedList.printList();
     }
 }
